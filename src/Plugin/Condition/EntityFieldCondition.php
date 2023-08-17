d5d29b2b906ae7f07e031a554d206708e2ee60f5
Clare Barton <cbarto8@emory.edu>
2023-08-17 00:07:32 +0000

    Drupal 10 compatibility.

--- src/Plugin/Condition/EntityFieldCondition.php
+++ src/Plugin/Condition/EntityFieldCondition.php
@@ -33,10 +33,6 @@ class EntityFieldCondition extends ConditionPluginBase implements ContainerFacto
   /**
    * Creates a new EntityField instance.
    *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
-   *   The entity type bundle info service.
    * @param array $configuration
    *   The plugin configuration, i.e. an array with configuration values keyed
    *   by configuration option name. The special key 'context' may be used to
@@ -46,6 +42,10 @@ class EntityFieldCondition extends ConditionPluginBase implements ContainerFacto
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
+   *   The entity type bundle info service.
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityTypeBundleInfoInterface $entity_type_bundle_info) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
